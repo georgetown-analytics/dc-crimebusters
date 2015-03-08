10793644d101567bbf8e84b03d740a0499e1df08
Andrew62 <awoizesko@gmail.com>
2015-03-08 12:25:57 -0400

    created config file. create your own path_variables.yaml for your personal settings and store in a new folder conf

--- crimebusters/conf.py
+++ crimebusters/conf.py
@@ -5,3 +5,23 @@ Created on Thu Mar 05 13:25:56 2015
 @author: andrew_woizesko
 """
 
+import os 
+import confire
+
+class CrimeBustersConfig(confire.Configuration):
+    """
+    configuration for the application. See documentation
+    here: http://pythonhosted.org/confire/
+    """
+    CONF_PATHS = [
+    "/etc/path_variables.yaml", 
+    os.path.expanduser('~/.path_variables.yaml'),
+    os.path.abspath(os.path.join("..","conf/path_variables.yaml"))
+    ]
+    
+    data = None
+    
+settings = CrimeBustersConfig.load()
+
+if __name__ == "__main__":
+    print settings
