9312900a26a476483e213b4440a8cc0568a979f7
Andrew62 <awoizesko@gmail.com>
2015-03-08 13:13:10 -0400

    added comments

--- crimebusters/spatial_regression.py
+++ crimebusters/spatial_regression.py
@@ -154,7 +154,6 @@ if __name__ == "__main__":
     shapefile= settings["shapefile"]
     dependent = "COUNT"
     independent = ["MEANDISTFR","MEANHomeIn", "MEANHomVal","MEANTrvlGr"]
-    #See GWR_Results.txt in the tests directory for spatial regression performance
     #Initial run with these features produced a model with an coefficient of 
     #determination (R^2) of ~0.08 --> not very good
     gwr = GeographicallyWeightedRegression(shapefile,dependent, independent,spatial_relationship="queen", out_summary=settings["spatial_regression_summary"])
