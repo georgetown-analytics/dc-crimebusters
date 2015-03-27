a1c20acb26f49d859e84f083f0391ea79fd60b4b
rdalbah <rdalbah@gmail.com>
2015-03-27 10:20:06 -0400

    Update classify.py

--- crimebusters/classify.py
+++ crimebusters/classify.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Feb 17 21:31:48 2015
-
 @author: andrew_woizesko
 """
 ###########################################################################
@@ -31,11 +30,11 @@ class EventClassifier(object):
         """
         
 		## Get the default model from the settings if it isn't passed in
-		model = model or settings.model
+	model = model or settings.model
 		
 		## Load the model from the pickle
-		with open(model, 'rb') as pkl:
-			self._classifier = pickle.load(pkl)
+	with open(model, 'rb') as pkl:
+		self._classifier = pickle.load(pkl)
 			
     
     def classify(self, instance):
@@ -45,10 +44,10 @@ class EventClassifier(object):
         user will encounter a crime on their trip
         """
         		
-		## Use the classifier to predict the probabilities of each crime
-		event_prob = self._classifier.predict_proba(instance)
+	## Use the classifier to predict the probabilities of each crime
+	event_prob = self._classifier.predict_proba(instance)
 		
-		return event_prob
+	return event_prob
     
     def explain(self):
         """
@@ -60,4 +59,3 @@ class EventClassifier(object):
 
 if __name__ == "__main__":
     classifier = EventClassifier()
-	
